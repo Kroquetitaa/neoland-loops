d492004f968a456b47a90060e4990f8ee59e58c4
Kroquetitaa <Kroquetita@gmail.com>
2022-07-14 22:20:22 +0200

    Iterator#3

---
+++ Exercise/iteraccion3.js
@@ -0,0 +1,10 @@
+
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar'];
+
+const destiny = list => {
+    for (const keys of list) {
+        console.log(keys);
+    }
+}
+
+destiny( placesToTravel );
